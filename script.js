5ba4148938b010d2caddefaa9b40d351e705c0ac
arvind272301 <134845043+arvind272301@users.noreply.github.com>
2024-03-23 12:30:20 -0700

    change

---
+++ script.js
@@ -0,0 +1,11 @@
+// ================================typed js ==================================
+// ================================ for uses amimation multiple text ==================================
+
+const typed = new Typed('.multiple-text', {
+    // strings: ['Frontend Developer', 'MERN Full Stack Developer','Java Full Stack Developer'],
+    strings: ['Arvind'],
+    typeSpeed: 200,
+    backSpeed: 200,
+    backDelay:1000,
+    loop: true
+});
