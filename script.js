7ae5c575a7de0f85cad9e8f40a919ebafab4f5a6
arvind272301 <134845043+arvind272301@users.noreply.github.com>
2024-03-23 13:49:42 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 const typed = new Typed('.multiple-text', {
     // strings: ['Frontend Developer', 'MERN Full Stack Developer','Java Full Stack Developer'],
-    strings: ['Arvind'],
+    strings: ['Pallvi '],
     typeSpeed: 200,
     backSpeed: 200,
     backDelay:1000,
